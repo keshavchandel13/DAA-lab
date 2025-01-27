dd081b8d8c9d8511a9690639fcff055e8da59a4f
Keshav Chandel <chandelkeshav4@gmail.com>
2025-01-27 09:24:54 +0530

    selection sort

---
+++ 05_SelectionSort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {64,25,12,22,11};
+    cout<<"Unsorted array\n";
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+    for(int i=0;i<5;i++){
+        int min = arr[i];
+        for(int j=i+1;j<5;j++){
+            if(min>arr[j]){
+                int temp = arr[j];
+                arr[j] = min;
+                min = temp;
+            }
+        }
+        arr[i] = min; 
+    }
+    cout<<"\nSorted Array\n";
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
