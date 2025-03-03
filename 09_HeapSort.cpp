c2a05b6eb9fc84678fdb4fd6be81899226661934
Keshav Chandel <chandelkeshav4@gmail.com>
2025-03-03 15:09:28 +0530

    heap sort

---
+++ 09_HeapSort.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+void heapify(int arr[], int n, int i){
+    int left = i*2+1;
+    int right = i*2+2;
+    int largest = i;
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+    if(largest!=i){
+        swap(arr[i],arr[largest]);
+        heapify(arr,n,largest);
+    }
+}
+void heapSort(int arr[], int n){
+    for(int i = n/2-1; i>=0;i--){
+        heapify(arr,n,i);
+    }
+    for(int i = n-1;i>0; i--){
+        swap(arr[0],arr[i]);
+        heapify(arr,i,0);
+    }
+
+
+}
+void printArray(int arr[], int n){
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+    int arr[] = {2,7,1,10};
+    printArray(arr, 4);
+    heapSort(arr,4);
+    cout<<"\n";
+    printArray(arr, 4);
+
+
+
+return 0;
+}
